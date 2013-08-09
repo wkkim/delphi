eeea732e367dc28b86edccfc8a1714cadffc492f
wkkim <kwk@embarcadero.kr>
2013-08-09 14:00:03 +0900

    First commit

---
+++ TestProject.dpr
@@ -0,0 +1,14 @@
+program TestProject;
+
+uses
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {Form5};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm5, Form5);
+  Application.Run;
+end.
